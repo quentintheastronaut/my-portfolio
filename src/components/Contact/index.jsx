6ecef39cc30c4a4cb46cd92123b29be73f2390fa
Nhat Quan <quan260402@gmail.com>
2022-03-13 22:16:28 +0700

    done contact

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 import './index.css'
 import { MdOutlineEmail } from 'react-icons/md'
-
+import { RiMessengerLine } from 'react-icons/ri'
+import { SiZalo } from 'react-icons/si'
 
 function Contact() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_kn78i8m', 'template_urmcvme', form.current, 'aWe3VIJCl3KKcTMHC')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+
+        e.target.reset();
+    };
+
+
     return (
         <section id="contact">
             <h5>Get In Touch</h5>
@@ -12,15 +30,15 @@ function Contact() {
             <div className="container contact__container">
                 <div className="contact__options">
                     <article className='contact__option'>
-                        <MdOutlineEmail />
+                        <MdOutlineEmail className='contact__option-icon' />
                         <h4>Email</h4>
                         <h5>quan.dang2604bku@hcmut.edu.vn</h5>
-                        <a href="mailto:quan.dang2604bku@hcmut.edu.vn">
+                        <a href="mailto:quan.dang2604bku@hcmut.edu.vn" target="_blank">
                             Send a message
                         </a>
                     </article>
                     <article className='contact__option'>
-                        <MdOutlineEmail />
+                        <RiMessengerLine className='contact__option-icon' />
                         <h4>Messenger</h4>
                         <h5>Nhật Quân</h5>
                         <a href="https://m.me/100007324064936">
@@ -28,16 +46,21 @@ function Contact() {
                         </a>
                     </article>
                     <article className='contact__option'>
-                        <MdOutlineEmail />
-                        <h4>Email</h4>
-                        <h5>quan.dang2604bku@hcmut.edu.vn</h5>
+                        <SiZalo className='contact__option-icon' />
+                        <h4>Zalo</h4>
+                        <h5>Đặng Nhật Quân: (+84)77 88 12024</h5>
                         <a href="mailto:quan.dang2604bku@hcmut.edu.vn">
                             Send a message
                         </a>
                     </article>
                 </div>
 
-                <form action=""></form>
+                <form action="" ref={form} onSubmit={sendEmail}>
+                    <input type="text" name='name' placeholder='Your full name' required />
+                    <input type="email" name='email' placeholder='Your email' required />
+                    <textarea name="message" rows="7" placeholder="Your message" required />
+                    <button type="submit" className='btn btn-primary' >Send message</button>
+                </form>
             </div>
         </section>
     )
